5c2745442ae5ec9badaac4dca64edc4f82a6b329
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-02-13 19:06:05 +0300

    Исправлено для 4 задачи

--- task4.py
+++ task4.py
@@ -15,12 +15,26 @@ def factor(num):
     return result
 
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, x, y = egcd(b % a, a)
-        return (g, y - (b // a) * x, x)
+def blocking(num, size_num, size_block):
+    return [int.from_bytes(num.to_bytes(size_num, 'big')[i * size_block: (i + 1) * size_block], 'big') for i in
+            range(size_num // size_block)]
+
+
+def decrypt(C, d, n):
+    bytes_val = blocking(C, 32, 4)
+    res = bytearray()
+    for num in bytes_val:
+        res += pow(num, d, n).to_bytes(8, 'big')
+    return int.from_bytes(res, 'big')
+
+
+def encrypt(P, e, n):
+    bytes_val = blocking(P, 64, 8)
+
+    res = bytearray()
+    for num in bytes_val:
+        res += pow(num, e, n).to_bytes(4, 'big')
+    return int.from_bytes(res, 'big')
 
 
 n = 471090785117207
@@ -28,14 +42,20 @@ e = 12377
 
 C = 314999112281065205361706341517321987491098667
 
+print('Зашифрованный текст:', C)
+
 p, q = factor(n)
 print('p =', p, ', q =', q)
 f = (p - 1) * (q - 1)
+print('f =', f)
+
 d = inverse(e, f)
+
 print('d =', d)
 
-P = pow(C, d, n)
-print(P)
+P = decrypt(C, d, n)
 
-# TODO: непонятно почему не совпадает зашифрованной вновь текст. Нужно найти проблему
-print(pow(P, e, n))
+print('Расшифрованый текст:', P)
+
+C = encrypt(P, e, n)
+print('<Проверка> Зашифрованный текст:', C)
