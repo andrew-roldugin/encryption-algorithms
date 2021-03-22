99388e191d4d753b79b2a4181008e17d2184fdbc
Владимир <eau120676@yandex.ru>
2021-03-22 12:43:05 +0300

    Почти сделана 3 задача

--- task3.py
+++ task3.py
@@ -1,4 +1,71 @@
-def random(U, M, C, p):
-    return (U * M + C) % p
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-# TODO надо че то сделать вообще то
+from scipy.stats import norm
+
+#M = 22695477
+M = 101
+C = 1
+p = 2 ** 32
+
+
+def get_random(U, M, C, p):
+    U_new = (U * M + C) % p
+    return U_new / p, U_new
+
+
+def get_random_array(length):
+    U = [0]
+    R = []
+    global M, C, p
+    for i in range(length - 1):
+        rand, U_new = get_random(U[-1], M, C, p)
+        R.append(rand)
+        U.append(U_new)
+
+    return R
+
+
+def get_L():
+    numbers = []
+    U = [0]
+    global M, C, p
+    R = []
+    rand, num = get_random(U[-1], M, C, p)
+    while num not in numbers:
+        U.append(num)
+        R.append(rand)
+        numbers.append(num)
+        rand, num = get_random(U[-1], M, C, p)
+        if len(R) > 10000:
+            print('Более 10000')
+            return len(U)
+
+    return len(R)
+
+
+print('Наибольшая длина неповторяющихся элементов: ', get_L())
+
+
+b = 0.99
+d = 0.01
+N = int((norm.ppf((1 - b) / 2) / (2 * d)) ** 2)
+
+random_array = get_random_array(N)
+
+sns_plot = sns.distplot(random_array)
+fig = sns_plot.get_figure()
+plt.show()
+
+x = [random_array[2 * i] for i in range(len(random_array) // 2)]
+y = [random_array[2 * i + 1] for i in range(len(random_array) // 2)]
+
+hist, x_edges, y_edges = np.histogram2d(x, y, bins=10)
+
+plt.imshow(hist, extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]])
+plt.xticks(ticks=x_edges)
+plt.xlabel('x')
+plt.yticks(ticks=y_edges)
+plt.ylabel('y')
+plt.show()
